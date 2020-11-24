07e5f1aac46953ac0f8c5255c666106d5f815b53
Junior Henrique <juniorhenrique030@outlook.com>
2020-11-24 19:28:28 -0300

    Criando posiçoes randomicas

--- javaScript.js
+++ javaScript.js
@@ -1,10 +1,27 @@
-var altura; 
+var altura;
 var largura;
 function ajustaTamanhoPalcoJogo() {
-     altura = window.innerHeight;
-     largura = window.innerWidth;
+    altura = window.innerHeight;
+    largura = window.innerWidth;
 }
 
 ajustaTamanhoPalcoJogo();
 
 
+function posicaoRondomica() {
+    var positionX = Math.floor(Math.random() * largura) -90;
+    var positionY = Math.floor(Math.random() * altura) - 90;
+
+    positionX = positionX < 0 ? 0 : positionX;
+    positionY = positionY < 0 ? 0 : positionY;
+
+    //criar o elemento html
+    var mosquito = document.createElement('img');
+    mosquito.src = 'imagens/mosca.png';
+    mosquito.className = 'mosca1';
+    mosquito.style.left = positionX + 'px';
+    mosquito.style.top = positionY + 'px';
+    mosquito.style.position = 'absolute';
+    document.body.appendChild(mosquito);
+
+}
