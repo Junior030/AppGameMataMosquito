70dad80a9c88e288ad358f8061c0a1cdc2fd6396
Junior Henrique <juniorhenrique030@outlook.com>
2020-11-24 19:40:26 -0300

    Tamanhos Randomicos

--- javaScript.js
+++ javaScript.js
@@ -9,7 +9,7 @@ ajustaTamanhoPalcoJogo();
 
 
 function posicaoRondomica() {
-    var positionX = Math.floor(Math.random() * largura) -90;
+    var positionX = Math.floor(Math.random() * largura) - 90;
     var positionY = Math.floor(Math.random() * altura) - 90;
 
     positionX = positionX < 0 ? 0 : positionX;
@@ -18,10 +18,24 @@ function posicaoRondomica() {
     //criar o elemento html
     var mosquito = document.createElement('img');
     mosquito.src = 'imagens/mosca.png';
-    mosquito.className = 'mosca1';
+    mosquito.className = tamanhoAleatorio();
     mosquito.style.left = positionX + 'px';
     mosquito.style.top = positionY + 'px';
     mosquito.style.position = 'absolute';
     document.body.appendChild(mosquito);
 
+    
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3);
+
+    switch (classe) {
+        case 0:
+            return 'mosca1';
+        case 1:
+            return 'mosca2';
+        case 2:
+            return 'mosca3';
+    }
 }
